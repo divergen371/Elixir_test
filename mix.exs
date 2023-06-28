b787499057311b3146c61610949974893389e933
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-29 00:15:04 +0900

    :alembic: 外部APIを叩く準備

--- mix.exs
+++ mix.exs
@@ -48,6 +48,7 @@ defmodule Basic.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
+      {:req, "~> 0.3"}
     ]
   end
 
