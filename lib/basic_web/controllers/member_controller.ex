05359b778cf6207a4e3e42d7469fd31bdb5b84ca
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-29 10:53:08 +0900

    :alembic: データをJSON化

---
+++ lib/basic_web/controllers/member_controller.ex
@@ -0,0 +1,13 @@
+defmodule BasicWeb.MemberController do
+  use BasicWeb, :controller
+
+  def index(conn, _p) do
+    conn
+    |> json(
+      "select * from members"
+      |> Db.query()
+      |> Db.columns_rows()
+      |> Enum.sort(fn current, next -> current["id"] < next["id"] end)
+    )
+  end
+end
