2d5c945d99969210efbab4b170f021e80773096e
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-29 14:53:10 +0900

    :alembic: :sparkles: データ追加機能

--- lib/basic_web/controllers/member_controller.ex
+++ lib/basic_web/controllers/member_controller.ex
@@ -3,11 +3,18 @@ defmodule BasicWeb.MemberController do
 
   def index(conn, _p) do
     conn
-    |>json(
+    |> json(
       "select *from members"
       |> Db.query()
       |> Db.columns_rows()
-      |> Enum.sort(fn current, next -> current["id"]< next["id"]end)
+      |> Enum.sort(fn current, next -> current["id"] < next["id"] end)
     )
+  end
+
+  def create(conn, p) do
+    "insert into members values('#{p["name"]}', #{p["age"]}, '#{p["team"]}', '#{p["position"]}')"
+    |> Db.query()
+
+    send_resp(conn, :created, "")
   end
 end
